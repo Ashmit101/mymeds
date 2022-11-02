54e75f0c7bf60a060564c33f21c8d2325c38be95
anju-chhetri <anju9chhetri@gmail.com>
2022-11-02 22:53:57 +0545

    implement Search input in app

--- lib/screens/more/search.dart
+++ lib/screens/more/search.dart
@@ -22,10 +22,89 @@ class _WebSearchState extends State<WebSearch> {
     return  Scaffold(
       appBar: AppBar(
         title: const Text('Search'),
+        
+        actions: [
+          IconButton(
+            onPressed: (){
+              showSearch(
+                context: context, 
+                delegate: SearchMedicine(),
+                );
+
+            }, 
+            icon: const Icon(Icons.search),
+            )
+        ],
       ),
-      body: const WebView(
-      initialUrl: 'https://www.drugs.com/',
-     ),
+    
+    //   body: const WebView(
+    //   initialUrl: 'https://www.drugs.com/',
+    //  ),
     );
   }
 }
+
+
+class SearchMedicine extends SearchDelegate{
+
+    List<String> Allsuggestions = [
+      'paracetamol',
+      'Otezla',
+      'Melatonin',
+      'Benzonatate',
+      'Otezla',
+      'Doxycycline',
+      'Hydroxychloroquine',
+      'Aspirin',
+      'Pantoprazole',
+    ];
+
+
+  @override
+  Widget? buildLeading(BuildContext context)=>IconButton(
+    onPressed: ()=>close(context, null),
+    icon:const Icon(Icons.arrow_back),
+    );
+
+  @override
+  List<Widget>? buildActions(BuildContext context)=>[
+    IconButton(
+        onPressed:(){
+          if(query.isEmpty){
+            close(context, null);
+          }else{
+          query = '';}
+        },
+        icon:const Icon(Icons.clear),
+       )
+  ];
+      @override
+  Widget buildResults(BuildContext context)=> WebView(
+      initialUrl: 'https://www.drugs.com/$query',
+     ); 
+  @override
+  Widget buildSuggestions(BuildContext context){
+
+    List<String> suggestions = Allsuggestions.where((searchResult){
+      final results = searchResult;
+      final input = query;
+      return results.contains(input);
+    }).toList();
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: ((context, index) {
+        final suggestion = suggestions[index];
+
+        return ListTile(
+          title: Text(suggestion),
+          onTap: (){
+            query = suggestion;
+            showResults(context);
+          },
+        );
+      }),
+    );
+  }
+
+} 
